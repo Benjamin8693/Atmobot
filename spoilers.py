fdf5efe0a4816948f75f2b2b46e18804f49358ea
Benjamin8693 <11221724+Benjamin8693@users.noreply.github.com>
2021-09-23 02:46:15 -0400

    spoilers: More debug and Discord fix

--- spoilers.py
+++ spoilers.py
@@ -46,7 +46,6 @@ class Spoilers(UpdateNotifier):
 
         # Force disable Discord or Twitter posts
         self.discord_post_override = False
-        self.discord_post_override = True
         self.twitter_post_override = False
 
     async def startup(self):
@@ -133,8 +132,13 @@ class Spoilers(UpdateNotifier):
                 goodbye_text = settings.get("spoiler_goodbye")
 
                 # Post it to Twitter
+                print("asking if we need to post")
                 if goodbye_text:
+                    print("posting to twitterrrr")
                     self.twitter_api.PostUpdate(status=goodbye_text, media="resources/goodbye.png")
+
+                    print("posting test greeting")
+                    self.twitter_api.PostUpdate(status=goodbye_text, media="resources/greetings.png")
 
                 print("{time} | SPOILERS: Update has been spoiled. Until next time!".format(time=await self.bot.get_formatted_time()))
 
